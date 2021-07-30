84e2e68f65b3a2fa6ccc7f1381970b760d6fec58
Adeshina Ibrahim <debowale01@gmail.com>
2021-07-30 19:23:38 +0100

    added css files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
+import Header from './components/Header'
+import Footer from './components/Footer'
+const App = () => {
   return (
     <>
+      <Header />
+      <main>
       <h1>Welcome to ShoePro</h1>
+      </main>
+      <Footer />
     </>
   );
 }
